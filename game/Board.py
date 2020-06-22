4bdb8d94eb75a3a6edece72dd266125ed5fe8ee0
gianlucanotaro <gianluca.notaro@students.fhnw.ch>
2020-06-22 22:59:29 +0200

    add structure board

---
+++ game/Board.py
@@ -0,0 +1,13 @@
+class board:
+    def __init__(self,pieces):
+        grid = [[]]
+
+    def draw(self):
+        pass
+
+    def refresh(self):
+        pass
+
+    def check(self):
+        pass
+
