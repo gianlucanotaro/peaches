bc898a2f5ca7365a5d4bb7284e81d95f09e1668d
gianlucanotaro <gianluca.notaro@students.fhnw.ch>
2020-07-13 15:29:21 +0200

    backup minimax structure

--- game/peaches_engine.py
+++ game/peaches_engine.py
@@ -283,3 +283,32 @@ class GameState:
                     in_check = True
                     checks.append((end_row, end_col, m[0], m[1]))
         return in_check, pins, checks
+
+    def minimax(self):
+        MAX, MIN = 1000, -1000
+
+        def minimax(depth, nodeIndex, maximizingPlayer,
+                    values, alpha, beta):
+
+            if depth == 3:
+                return values[nodeIndex]
+
+            if maximizingPlayer:
+                best = MIN
+                for i in range(0, 2):
+                    val = minimax(depth + 1, nodeIndex * 2 + i, False, values, alpha, beta)
+                    best = max(best, val)
+                    alpha = max(alpha, best)
+                    if beta <= alpha:
+                        break
+                return best
+            else:
+                best = MAX
+                for i in range(0, 2):
+                    val = minimax(depth + 1, nodeIndex * 2 + i, True, values, alpha, beta)
+                    best = min(best, val)
+                    beta = min(beta, best)
+                    if beta <= alpha:
+                        break
+                return best
+
